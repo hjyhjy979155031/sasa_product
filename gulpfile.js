aae0b7996bf072c90c262cd3fe308fac9a0287ed
hjyhjy979155031 <979155031@qq.com>
2018-08-24 11:52:39 +0800

    初步配置项目

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+var babel = require("gulp-babel");
+
+gulp.task('js',()=>{
+	gulp.src('./src/js/*.js').pipe(rename({
+		suffix:'.min'
+	})).pipe(babel({
+		presets:["env"]
+	})).pipe(uglify()).pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('sass',()=>{
+	gulp.src('./src/scss/**/*.scss').pipe(sass()).pipe(rename({
+		suffix:'.min'
+	})).pipe(gulp.dest('dist/css'));
+})
+
+gulp.task('default',()=>{
+//	gulp.watch('./src/js/*.js',['js']);
+	gulp.watch('./src/scss/*.scss',['sass']);
+})
